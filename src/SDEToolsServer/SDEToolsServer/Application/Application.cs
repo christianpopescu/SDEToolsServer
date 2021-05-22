53734485a29a1b3c945f2d4048781453ad28802e
Christian Popescu <christian.popescu@outlook.com>
2021-05-22 08:09:06 +0200

    Added configuration section for port

--- src/SDEToolsServer/SDEToolsServer/Application/Application.cs
+++ src/SDEToolsServer/SDEToolsServer/Application/Application.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using log4net;
+using SDEToolsServer.Config;
 
 namespace SDEToolsServer.Application
 {
@@ -15,9 +17,24 @@ namespace SDEToolsServer.Application
     public class Application
     {
         private static readonly ILog log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
+        // To do: Create a more generic configuration intialization (builder or injector)
+        public Application()
+        {
+            Config = ConfigurationManager.OpenExeConfiguration(
+                    ConfigurationUserLevel.None); 
+        }
+
+        public System.Configuration.Configuration Config
+        {
+            get; private set;
+        }
+
         public int Run()
         {
             log4net.Config.XmlConfigurator.Configure();
+            var ssc = Config.GetSection("SocketServer") as SocketServerConfigSection;
+            log.Info("PORT" + ssc.Port);
             ThreadContext.Stacks["NDC"].Push("Application");
             log.Info("----- SDEToolsServer - Started! ------");
             return 0;
---
+++ src/SDEToolsServer/SDEToolsServer/Config/SocketServerConfig.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SDEToolsServer.Config
+{
+    public class SocketServerConfigSection : ConfigurationSection
+    {
+        [ConfigurationProperty("port",
+            IsRequired = true,
+            IsKey = false)]
+        public string Port
+        {
+
+            get
+            {
+                return (string)this["port"];
+            }
+            set
+            {
+                this["port"] = value;
+            }
+        }
+    }
+}
